11acd046fa9aa7d33a3903f5ec68e8730e2ff431
PavankumarKokane <pavankokane2013@gmail.com>
2023-10-19 15:35:19 +0530

    fetch new by api call

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewItem extends Component {
   render() {
     let { title, description, url, img_url } = this.props;
     return (
-        <div className="card">
-            <img loading='lazy' height={225} width={375} src={img_url} className="card-img-top" alt={title} />
-            <div className="card-body">
-            <h5 className="card-title">{title.substring(0, 50)}...</h5>
-            <p className="card-text">{description.substring(0, 150)}...</p>
-            <a href={url} className="btn btn-primary">View More</a>
-            </div>
+      <div className="card">
+        <img
+          loading="lazy"
+          height={225}
+          width={375}
+          src={img_url?img_url:"https://via.placeholder.com/375x225"}
+          className="card-img-top"
+          alt={title}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title?title.substring(0, 50):" "}...</h5>
+          <p className="card-text">{description?description.substring(0, 150):" "}...</p>
+          <a
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            View More
+          </a>
         </div>
-    )
+      </div>
+    );
   }
 }
